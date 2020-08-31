b51ed9e7aa59513c92d90b7e3786d682dafe83d4
Jarno Lehtinen <lehtinen@sci.fi>
2020-08-31 13:43:17 +0300

    Create boot.py

---
+++ micropython/boot.py
@@ -0,0 +1,10 @@
+import ujson
+with open('wifi_config.json') as infile:
+  conf = ujson.load(infile)
+import network
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect(conf['ssid'], conf['passwd'])
+del conf
+
+from utils import *
