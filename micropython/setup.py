8c696efc998b1d1f855ce37b98729c07177a7909
Jarno Lehtinen <lehtinen@sci.fi>
2020-09-01 18:49:17 +0300

    Update setup.py

--- micropython/setup.py
+++ micropython/setup.py
@@ -27,10 +27,12 @@ def wget(url, filename):
   s.close()
 
 def install():
+  print('Loading WiFi-settings from wifi_config.json...')
   with open('wifi_config.json') as infile:
     conf = ujson.load(infile)
   import network
   import utime
+  print('Starting network...')
   sta_if = network.WLAN(network.STA_IF)
   sta_if.active(True)
   sta_if.connect(conf['ssid'], conf['passwd'])
@@ -39,7 +41,11 @@ def install():
     print("Waiting for connection...")
     while not sta_if.isconnected():
       utime.sleep(1)
+  print('Downloading mqtt.py...')
   wget('https://raw.githubusercontent.com/mcgurk/ESP8266_MQTT_ledmatrixdisplay/master/micropython/ESP_MQTT_ledmatrixdisplay.py', 'mqtt.py')
+  print('Downloading boot.py...')
   wget('https://raw.githubusercontent.com/mcgurk/ESP8266_MQTT_ledmatrixdisplay/master/micropython/boot.py', 'boot.py')
+  print('Downloading utils.py...')
   wget('https://raw.githubusercontent.com/mcgurk/ESP8266_MQTT_ledmatrixdisplay/master/micropython/utils.py', 'utils.py')
+  print('Downloading max7219.py...')
   wget('https://raw.githubusercontent.com/mcauser/micropython-max7219/master/max7219.py', 'max7219.py')
